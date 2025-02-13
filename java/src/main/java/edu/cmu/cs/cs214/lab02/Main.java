899b93eba9ad7b7dc4dcaa0329a51b9e61b12b2d
BJ.A.J. <107040086+Bayraa0227@users.noreply.github.com>
2025-02-13 11:28:04 +0800

    Main класст бусад классын хэрэгжүүлэлтийг шалгах код нэмсэн

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,7 +1,9 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 /** Ажиллуулах класс. */
 public class Main {
@@ -11,10 +13,20 @@ public class Main {
    * @param args Аргументууд
    */
   public static void main(String[] args) {
-    Shape rectangle = new Rectangle(2, 3);
+    Shape rectangle1 = new Rectangle(3, 4);
+    Renderer rendererRect1 = new Renderer(rectangle1);
+    rendererRect1.draw(); // 12
 
-    Renderer renderer = new Renderer(rectangle);
+    Rectangle rectangle2 = new Rectangle(10, 34.5);
+    Renderer rendererRect2 = new Renderer(rectangle2);
+    rendererRect2.draw(); // 345
 
-    renderer.draw();
+    Shape circle1 = new Circle(10);
+    Renderer rendererCirc1 = new Renderer(circle1);
+    rendererCirc1.draw(); // 314.16
+
+    Shape square1 = new Square(8);
+    Renderer rendererSq1 = new Renderer(square1);
+    rendererSq1.draw(); // 64
   }
 }
